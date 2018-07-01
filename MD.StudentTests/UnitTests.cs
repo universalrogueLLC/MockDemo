952d0e344cc9c47b0d3ef4009a410f390e85d3cd
Scott Palmateer <scott@universalrogue.com>
2018-07-01 14:39:58 -0400

    completed demo application

--- MD.StudentTests/UnitTests.cs
+++ MD.StudentTests/UnitTests.cs
@@ -9,6 +9,9 @@ namespace MD.StudentTests
     [TestClass]
     public class UnitTests
     {
+        private double _calculatorOperand1;
+        private double _calculatorOperand2;
+
         private string _loggedOperation;
         private double _loggedOperand1;
         private double _loggedOperand2;
@@ -29,14 +32,16 @@ namespace MD.StudentTests
             student.AddTwoNumbers(operand1, operand2);
 
             // verify that the correct calculator function was called, with the correct arguments, exactly once
-            calculatorMock.Verify(x => x.Add(operand1, operand2), Times.AtMostOnce());
+            calculatorMock.Verify(x => x.Add(operand1, operand2), Times.Exactly(1));
+            Assert.AreEqual(operand1, this._calculatorOperand1, "unexpected OPERAND1 supplied to calculator");
+            Assert.AreEqual(operand2, this._calculatorOperand2, "unexpected OPERAND2 supplied to calculator");
 
             // verify that the result was logged correctly, exactly once
-            resultServiceMock.Verify(x => x.LogResult(expectedOperation, operand1, operand2, expectedResult), Times.AtMostOnce);
-            Assert.AreEqual(expectedOperation, this._loggedOperation);
-            Assert.AreEqual(operand1, this._loggedOperand1);
-            Assert.AreEqual(operand2, this._loggedOperand2);
-            Assert.AreEqual(expectedResult, this._loggedResult);
+            resultServiceMock.Verify(x => x.LogResult(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>()), Times.Exactly(1));
+            Assert.AreEqual(expectedOperation, this._loggedOperation, "unexpected OPERATION logged");
+            Assert.AreEqual(operand1, this._loggedOperand1, "unexpected OPERAND1 logged");
+            Assert.AreEqual(operand2, this._loggedOperand2, "unexpected OPERAND2 logged");
+            Assert.AreEqual(expectedResult, this._loggedResult, "unexpected RESULT logged");
 
             // verify that no exception was ever logged
             resultServiceMock.Verify(x => x.LogException(It.IsAny<string>()), Times.Never);
@@ -57,13 +62,19 @@ namespace MD.StudentTests
             student.SubtractTwoNumbers(operand1, operand2);
 
             // verify that the correct calculator function was called, with the correct arguments, exactly once
-            calculatorMock.Verify(x => x.Subtract(operand1, operand2), Times.AtMostOnce());
+            calculatorMock.Verify(x => x.Subtract(operand1, operand2), Times.Exactly(1));
+            Assert.AreEqual(operand1, this._calculatorOperand1, "unexpected OPERAND1 supplied to calculator");
+            Assert.AreEqual(operand2, this._calculatorOperand2, "unexpected OPERAND2 supplied to calculator");
 
             // verify that the result was logged correctly, exactly once
-            resultServiceMock.Verify(x => x.LogResult(expectedOperation, operand1, operand2, expectedResult), Times.AtMostOnce());
+            resultServiceMock.Verify(x => x.LogResult(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>()), Times.Exactly(1));
+            Assert.AreEqual(expectedOperation, this._loggedOperation, "unexpected OPERATION logged");
+            Assert.AreEqual(operand1, this._loggedOperand1, "unexpected OPERAND1 logged");
+            Assert.AreEqual(operand2, this._loggedOperand2, "unexpected OPERAND2 logged");
+            Assert.AreEqual(expectedResult, this._loggedResult, "unexpected RESULT logged");
 
             // verify that no exception was ever logged
-            resultServiceMock.Verify(x => x.LogException(It.IsAny<string>()), Times.Never());
+            resultServiceMock.Verify(x => x.LogException(It.IsAny<string>()), Times.Never);
         }
 
         [TestMethod]
@@ -81,13 +92,19 @@ namespace MD.StudentTests
             student.MultiplyTwoNumbers(operand1, operand2);
 
             // verify that the correct calculator function was called, with the correct arguments, exactly once
-            calculatorMock.Verify(x => x.Multiply(operand1, operand2), Times.AtMostOnce());
+            calculatorMock.Verify(x => x.Multiply(operand1, operand2), Times.Exactly(1));
+            Assert.AreEqual(operand1, this._calculatorOperand1, "unexpected OPERAND1 supplied to calculator");
+            Assert.AreEqual(operand2, this._calculatorOperand2, "unexpected OPERAND2 supplied to calculator");
 
             // verify that the result was logged correctly, exactly once
-            resultServiceMock.Verify(x => x.LogResult(expectedOperation, operand1, operand2, expectedResult), Times.AtMostOnce());
+            resultServiceMock.Verify(x => x.LogResult(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>()), Times.Exactly(1));
+            Assert.AreEqual(expectedOperation, this._loggedOperation, "unexpected OPERATION logged");
+            Assert.AreEqual(operand1, this._loggedOperand1, "unexpected OPERAND1 logged");
+            Assert.AreEqual(operand2, this._loggedOperand2, "unexpected OPERAND2 logged");
+            Assert.AreEqual(expectedResult, this._loggedResult, "unexpected RESULT logged");
 
             // verify that no exception was ever logged
-            resultServiceMock.Verify(x => x.LogException(It.IsAny<string>()), Times.Never());
+            resultServiceMock.Verify(x => x.LogException(It.IsAny<string>()), Times.Never);
         }
 
         [TestMethod]
@@ -105,13 +122,19 @@ namespace MD.StudentTests
             student.DivideTwoNumbers(operand1, operand2);
 
             // verify that the correct calculator function was called, with the correct arguments, exactly once
-            calculatorMock.Verify(x => x.Divide(operand1, operand2), Times.AtMostOnce());
+            calculatorMock.Verify(x => x.Divide(operand1, operand2), Times.Exactly(1));
+            Assert.AreEqual(operand1, this._calculatorOperand1, "unexpected OPERAND1 supplied to calculator");
+            Assert.AreEqual(operand2, this._calculatorOperand2, "unexpected OPERAND2 supplied to calculator");
 
             // verify that the result was logged correctly, exactly once
-            resultServiceMock.Verify(x => x.LogResult(expectedOperation, operand1, operand2, expectedResult), Times.AtMostOnce());
+            resultServiceMock.Verify(x => x.LogResult(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>()), Times.Exactly(1));
+            Assert.AreEqual(expectedOperation, this._loggedOperation, "unexpected OPERATION logged");
+            Assert.AreEqual(operand1, this._loggedOperand1, "unexpected OPERAND1 logged");
+            Assert.AreEqual(operand2, this._loggedOperand2, "unexpected OPERAND2 logged");
+            Assert.AreEqual(expectedResult, this._loggedResult, "unexpected RESULT logged");
 
             // verify that no exception was ever logged
-            resultServiceMock.Verify(x => x.LogException(It.IsAny<string>()), Times.Never());
+            resultServiceMock.Verify(x => x.LogException(It.IsAny<string>()), Times.Never);
         }
 
         [TestMethod]
@@ -122,18 +145,25 @@ namespace MD.StudentTests
 
             var calculatorMock = BuildCalculatorMock();
             var resultServiceMock = BuildResultService();
-            var student = new GoodStudent(calculatorMock.Object, resultServiceMock.Object);
+            var student = BuildStudent(calculatorMock.Object, resultServiceMock.Object);
 
             student.DivideTwoNumbers(operand1, operand2);
 
             // verify that the correct calculator function was called, with the correct arguments, exactly once
-            calculatorMock.Verify(x => x.Divide(operand1, operand2), Times.AtMostOnce());
+            calculatorMock.Verify(x => x.Divide(operand1, operand2), Times.Exactly(1));
+            Assert.AreEqual(operand1, this._calculatorOperand1, "unexpected OPERAND1 supplied to calculator");
+            Assert.AreEqual(operand2, this._calculatorOperand2, "unexpected OPERAND2 supplied to calculator");
 
             // verify that no result was logged
             resultServiceMock.Verify(x => x.LogResult(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>()), Times.Never());
 
             // verify that the exception was logged, exactly once.
-            resultServiceMock.Verify(x => x.LogException(It.IsAny<string>()), Times.AtMostOnce());
+            resultServiceMock.Verify(x => x.LogException(It.IsAny<string>()), Times.Exactly(1));
+        }
+
+        private IStudent BuildStudent(ICalculator calculator, IResultService resultService)
+        {
+            return new GoodStudent(calculator, resultService);
         }
 
         private Mock<ICalculator> BuildCalculatorMock()
@@ -141,17 +171,22 @@ namespace MD.StudentTests
             var mock = new Mock<ICalculator>();
 
             mock.Setup(x => x.Add(It.IsAny<double>(), It.IsAny<double>()))
+                .Callback((double op1, double op2) => { HandleCalculatorCallback(op1, op2); })
                 .Returns((double x, double y) => x + y);
 
             mock.Setup(x => x.Subtract(It.IsAny<double>(), It.IsAny<double>()))
+                .Callback((double op1, double op2) => { HandleCalculatorCallback(op1, op2); })
                 .Returns((double x, double y) => x - y);
 
             mock.Setup(x => x.Multiply(It.IsAny<double>(), It.IsAny<double>()))
+                .Callback((double op1, double op2) => { HandleCalculatorCallback(op1, op2); })
                 .Returns((double x, double y) => x * y);
 
             mock.Setup(x => x.Divide(It.IsAny<double>(), 0.0))
+                .Callback((double op1, double op2) => { HandleCalculatorCallback(op1, op2); })
                 .Throws(new DivideByZeroException());
             mock.Setup(x => x.Divide(It.IsAny<double>(), It.IsNotIn<double>(0)))
+                .Callback((double op1, double op2) => { HandleCalculatorCallback(op1, op2); })
                 .Returns((double x, double y) => x / y);
 
             return mock;
@@ -162,20 +197,23 @@ namespace MD.StudentTests
             var mock = new Mock<IResultService>();
 
             mock.Setup(x => x.LogResult(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>(), It.IsAny<double>()))
-                .Callback((string operation, double op1, double op2, double result) =>
-                {
-                    this._loggedOperation = operation;
-                    this._loggedOperand1 = op1;
-                    this._loggedOperand2 = op2;
-                    this._loggedResult = result;
-                });
+                .Callback((string operation, double op1, double op2, double result) => { HandleResultServiceCallback(operation, op1, op2, result); });
 
             return mock;
         }
 
-        private IStudent BuildStudent(ICalculator calculator, IResultService resultService)
+        private void HandleCalculatorCallback(double op1, double op2)
         {
-            return new GoodStudent(calculator, resultService);
+            this._calculatorOperand1 = op1;
+            this._calculatorOperand2 = op2;
+        }
+
+        private void HandleResultServiceCallback(string operation, double op1, double op2, double result)
+        {
+            this._loggedOperation = operation;
+            this._loggedOperand1 = op1;
+            this._loggedOperand2 = op2;
+            this._loggedResult = result;
         }
     }
 }
